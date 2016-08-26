44d50f07c6f5987f8cfb2b67513c01984ebb8ce2
Scott Liu <scott.liu@marinsoftware.com>
2016-08-26 12:15:12 -0700

    week2 update

--- lectureCode_l15-1.py
+++ lectureCode_l15-1.py
@@ -1,14 +1,14 @@
-import random, pylab
-#set line width
-pylab.rcParams['lines.linewidth'] = 6
-#set font size for titles 
-pylab.rcParams['axes.titlesize'] = 20
-#set font size for labels on axes
-pylab.rcParams['axes.labelsize'] = 20
-#set size of numbers on x-axis
-pylab.rcParams['xtick.major.size'] = 5
-#set size of numbers on y-axis
-pylab.rcParams['ytick.major.size'] = 5
+# import random, pylab
+# #set line width
+# pylab.rcParams['lines.linewidth'] = 6
+# #set font size for titles 
+# pylab.rcParams['axes.titlesize'] = 20
+# #set font size for labels on axes
+# pylab.rcParams['axes.labelsize'] = 20
+# #set size of numbers on x-axis
+# pylab.rcParams['xtick.major.size'] = 5
+# #set size of numbers on y-axis
+# pylab.rcParams['ytick.major.size'] = 5
 
 
 def flip(numFlips):
@@ -45,13 +45,27 @@ def flipPlot(minExp, maxExp):
     pylab.ylabel('Heads/Tails')
     pylab.plot(xAxis, ratios)
 
-##random.seed(0)
+# random.seed(0)
 ##flipPlot(4, 20)
 ##pylab.show()
 
 
 
+def stdDev(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    return (tot/len(X))**0.5
 
+
+def runTrial(numFlips):
+    numHeads = 0
+    for n in range(numFlips):
+        if random.random() < 0.5:
+            numHeads += 1
+    numTails = numFlips - numHeads
+    return numHeads, numTails
 
 
 
@@ -103,9 +117,40 @@ def flipPlot(minExp, maxExp):
     pylab.plot(xAxis, ratios, 'bo')
     pylab.semilogx()
     
-random.seed(0)
-flipPlot(4, 20)
-pylab.show()
+# random.seed(0)
+# flipPlot(4, 20)
+# pylab.show()
+
+
+def stdDevOfLengths(L):
+    """
+    L: a list of strings
+
+    returns: float, the standard deviation of the lengths of the strings,
+      or NaN if L is empty.
+    """
+    if not L:
+        return 'NaN'
+    else:
+        len1 = []
+        for i in L :
+            len1.append(len(i))
+
+        mean = sum(len1)/float(len(L))
+        tot = 0.0
+        for x in len1:
+            tot += (x - mean)**2
+        return (tot/len(L))**0.5
+
+L = ['apples', 'oranges', 'kiwis', 'pineapples']
+print stdDevOfLengths(L)
+L = []
+print stdDevOfLengths(L)
+
+
+
+
+
 
 
 
